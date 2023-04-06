b980e3c4e56c3b730b567f34cdeb212970c8fcf4
Vinicius Goncalves <viniciusfod@gmail.com>
2023-04-06 01:10:25 -0300

    mid page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { AiFillCheckCircle, AiOutlineCheckCircle } from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -97,34 +96,28 @@ export default function Home() {
           <h1 className="font-medium text-xl text-[#55C5E9]">
             estamos bem preparados para todos
           </h1>
-          <h2 className="font-medium text-6xl">Por que escolher-nos?</h2>
+          <h2 className="font-medium text-6xl mb-16">Por que escolher-nos?</h2>
         </div>
       </section>
-      <section className="flex justify-center uppercase">
-        <div className="flex flex-wrap justify-center">
-          <div>
-            <div className="flex">
-              <Image width={64} height={64} src="/muscleBlue.png" alt="" />
-              <h1 className="font-medium text-4xl w-[500px] text-[#FCFCFC]">
-                verdadeira atmosfera de academia
-              </h1>
-            </div>
-            <p className="font-normal ml-16 mb-16 text-base max-h-32 max-w-md text-[#fcfcfc9c]">
+      <section className="">
+        <ul className="text-slate-50 flex flex-wrap justify-center items-center w-full">
+          <li className="list-image-[url(/musculo.svg)] max-w-[508px] m-8">
+            <h1 className="font-medium text-4xl text-[#FCFCFC]">
+              verdadeira atmosfera de academia
+            </h1>
+            <p className="font-normal text-base text-[#fcfcfc9c]">
               Seja você um iniciante ou um atleta experiente, você se sentirá em
               casa em nosso ambiente inclusivo e acolhedor. Acreditamos que uma
               atmosfera de verdadeira academia é aquela que promove o
               crescimento e o desenvolvimento pessoal, e trabalhamos arduamente
               para criar esse tipo de cultura todos os dias.
             </p>
-          </div>
-          <div>
-            <div className="flex">
-              <Image width={64} height={64} src="/muscleBlue.png" alt="" />
-              <h1 className="font-medium text-4xl w-[500px] text-[#FCFCFC]">
-                todos os tipos de equipamentos
-              </h1>
-            </div>
-            <p className="font-normal text-base ml-16 max-h-32 max-w-md text-[#fcfcfc9c]">
+          </li>
+          <li className="list-image-[url(/gym-svgrepo-com.svg)] max-w-[508px] m-8">
+            <h1 className="font-medium text-4xl text-[#FCFCFC]">
+              todos os tipos de equipamentos
+            </h1>
+            <p className="font-normal text-base text-[#fcfcfc9c]">
               Nossa academia está completamente equipada com os mais recentes e
               melhores equipamentos fitness. Desde máquinas de cardio até pesos
               livres, temos tudo o que você precisa para alcançar seus objetivos
@@ -132,40 +125,32 @@ export default function Home() {
               treinamento intervalado de alta intensidade, encontrará o
               equipamento perfeito para seus treinos.
             </p>
-          </div>
-        </div>
-        <div className="flex flex-wrap justify-center">
-          <div>
-            <div className="flex">
-              <Image width={64} height={64} src="/muscleBlue.png" alt="" />
-              <h1 className="font-medium text-4xl  text-[#FCFCFC]">
-                instrutores experientes
-              </h1>
-            </div>
-            <p className="font-normal text-base ml-16 mb-16 max-h-32 max-w-md text-[#fcfcfc9c]">
+          </li>
+          <li className="list-image-[url(/instrutor.svg)] max-w-[508px] m-8">
+            <h1 className="font-medium text-4xl text-[#FCFCFC]">
+              instrutores experientes
+            </h1>
+            <p className="font-normal text-base text-[#fcfcfc9c]">
               Nossos instrutores são realmente incríveis! Com anos de
               experiência e uma paixão pelo fitness, eles estão dedicados a
               ajudar nossos membros a alcançarem seus objetivos. Se você quer
               perder peso, ganhar músculos ou simplesmente melhorar sua saúde e
               bem-estar em geral, nossos instrutores estão aqui para ajudar.
             </p>
-          </div>
-          <div>
-            <div className="flex">
-              <Image width={64} height={64} src="/muscleBlue.png" alt="" />
-              <h1 className="font-medium text-4xl w-[600px] text-[#FCFCFC]">
-                Nossa academia possui um ambiente acolhedor e profissional
-              </h1>
-            </div>
-            <p className="font-normal text-base ml-16 max-h-32 max-w-md text-[#fcfcfc9c]">
+          </li>
+          <li className="list-image-[url(/levant.svg)] max-w-[508px] m-8">
+            <h1 className="font-medium text-4xl text-[#FCFCFC]">
+              acolhedor e profissional
+            </h1>
+            <p className="font-normal text-base text-[#fcfcfc9c]">
               Nossa academia se orgulha de ser um lar longe de casa para muitos
               atletas profissionais. Nós entendemos as exigências de
               treinamentos intensos e competições, e nos esforçamos para
               fornecer as melhores instalações e suporte possível para ajudar
               nossos membros atletas a atingir seu máximo potencial.
             </p>
-          </div>
-        </div>
+          </li>
+        </ul>
       </section>
       <section className="flex justify-center text-[#FCFCFC]">
         <div className="flex justify-center items-center content-center flex-col mt-36 mb-12">
@@ -178,8 +163,8 @@ export default function Home() {
       <section className="flex justify-center text-[#FCFCFC]">
         <div className="flex justify-center items-center gap-8 ">
           <div className="flex flex-col justify-center items-center content-center text-center bg-[#FFFFFF1A] rounded-lg h-[526px] w-[352px]">
-            <h1 className="m-8 font-medium text-3xl">BÁSICO</h1>
-            <h2 className="m-8 font-semibold text-5xl ">
+            <h1 className="m-6 font-medium text-3xl">BÁSICO</h1>
+            <h2 className="m-6 font-semibold text-5xl ">
               R$20
               <strong className="font-normal text-base text-[#FCFCFCCC]">
                 /mes
@@ -239,8 +224,8 @@ export default function Home() {
             </button>
           </div>
           <div className="flex flex-col justify-center items-center content-center text-center bg-[#FFFFFF1A] rounded-lg h-[526px] w-[352px]">
-            <h1 className="m-8 font-medium text-3xl">VIP</h1>
-            <h2 className="m-8 font-semibold text-5xl ">
+            <h1 className="m-6 font-medium text-3xl">VIP</h1>
+            <h2 className="m-6 font-semibold text-5xl ">
               R$20
               <strong className="font-normal text-base text-[#FCFCFCCC]">
                 /mes
